8efb604b327b6eca92816dcfc6c463043cd8e173
Askhid <juansebastiandennytjamalla@gmail.com>
2022-06-11 13:45:14 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "game.h"
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	game defender_3;
+	srand(time(NULL));
+	defender_3.run();
+}
